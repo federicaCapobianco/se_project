4d2592d2d40a1b2624b3fe33dab47ff72bee63f2
emilia <e.delpezzo@studenti.unisa.it>
2022-11-22 15:16:26 +0100

    fix-Context

--- src/main/java/diem/unisa/softwareengineering/tools/Context.java
+++ src/main/java/diem/unisa/softwareengineering/tools/Context.java
@@ -1,18 +1,22 @@
 package diem.unisa.softwareengineering.tools;
 
+import javafx.scene.shape.Shape;
+
 public class Context {
     //add an instance of the Shape class
-    //private Shape shape;
+    private Shape shape;
 
     //add an initializer for the Context class
     public Context() {
-        //shape = new Line(); //default shape is our line class
+        //shape = new DrawableLine(); //default shape is our line class
+
     }
 
     //add a method changeState that instantiates the shape as for the passed parameter
-    //public void changeState(Shape state){
-    //    shape = new Shape(); //this will be line, circle, rectangle, etc.
-    //}
+    public void changeState(Shape state){
+       shape = state; //this will be line, circle, rectangle, etc.
+
+    }
 
 
 }
