71a85247981048b919774b677ca94f2e20b64c50
federicaCapobianco <capobianco.fede01@gmail.com>
2022-11-22 12:10:06 +0100

    add-Context class add-Context initializer add-Context changeState method

---
+++ src/main/java/diem/unisa/softwareengineering/tools/Context.java
@@ -0,0 +1,18 @@
+package diem.unisa.softwareengineering.tools;
+
+public class Context {
+    //add an instance of the Shape class
+    //private Shape shape;
+
+    //add an initializer for the Context class
+    public Context() {
+        //shape = new Line(); //default shape is our line class
+    }
+
+    //add a method changeState that instantiates the shape as for the passed parameter
+    //public void changeState(Shape state){
+    //    shape = new Shape(); //this will be line, circle, rectangle, etc.
+    //}
+
+
+}
