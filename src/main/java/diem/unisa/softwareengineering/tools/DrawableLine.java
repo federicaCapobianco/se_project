eb80dbf769eaf85e2a6df71dfc919b4f385ef193
emilia <e.delpezzo@studenti.unisa.it>
2022-11-22 15:41:29 +0100

    add-DrawableLine class

---
+++ src/main/java/diem/unisa/softwareengineering/tools/DrawableLine.java
@@ -0,0 +1,13 @@
+package diem.unisa.softwareengineering.tools;
+
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Shape;
+
+public class DrawableLine extends Shape implements Drawable{
+
+    @Override
+    public Shape draw(double xS, double yS, double xE, double yE) {
+        Line line = new Line(xS, yS, xE, yE);
+        return line;
+    }
+}
