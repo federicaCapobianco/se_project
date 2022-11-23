05724f824061f51585e14a31294b95228a67c6c2
fpastorepalumbo <f.pastorepalumbo@studenti.unisa.it>
2022-11-23 10:54:44 +0100

    add-override draw method

--- src/main/java/diem/unisa/softwareengineering/tools/DrawableEllipse.java
+++ src/main/java/diem/unisa/softwareengineering/tools/DrawableEllipse.java
@@ -3,12 +3,14 @@ import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Shape;
 
+import static java.lang.Math.abs;
+
 public class DrawableEllipse extends DrawableShape implements Drawable{
 
     private Shape ellipse;
     @Override
     public Shape draw(double xS, double yS, double xE, double yE) {
-        this.ellipse = new Ellipse(xS, yS, xE, yE);
+        this.ellipse = new Ellipse(xS, yS, abs(xS-xE), abs(yS-yE)); //xS, yS, xE, yE
         return ellipse;
     }
 }
