3b3cb2263195b26fb9fc41a38bd2fb3c18e6a69d
federicaCapobianco <capobianco.fede01@gmail.com>
2022-11-23 13:05:28 +0100

    fix-test configuration

---
+++ test/diem/unisa/softwareengineering/tools/ContextTest.java
@@ -0,0 +1,13 @@
+package diem.unisa.softwareengineering.tools;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContextTest {
+
+    @Test
+    void changeState() {
+        assert(true);
+    }
+}
