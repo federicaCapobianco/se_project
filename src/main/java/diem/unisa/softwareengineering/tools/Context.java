7d2695c0568c422459743cc188d996ec3f333bac
federicaCapobianco <capobianco.fede01@gmail.com>
2022-11-23 23:47:15 +0100

    refactor-moved save and load methods in a custom class

--- src/main/java/diem/unisa/softwareengineering/tools/Context.java
+++ src/main/java/diem/unisa/softwareengineering/tools/Context.java
@@ -2,8 +2,6 @@ package diem.unisa.softwareengineering.tools;
 
 import javafx.scene.Node;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.LinearGradient;
-import javafx.scene.paint.RadialGradient;
 import javafx.scene.shape.Shape;
 
 import java.awt.*;
@@ -23,6 +21,16 @@ public class Context {
     private DrawableShape shape;
 
     private double xS,xE,yS,yE;
+
+    //add a method changeState that instantiates the shape as for the passed parameter
+    public void changeState(DrawableShape state){
+        shape = state; //this will be line, circle, rectangle, etc.
+    }
+
+    //add a method draw that calls the draw method of the shape class
+    public Shape draw(){
+        return shape.draw(xS,yS,xE,yE);
+    }
 
     public double getxS() {
         return xS;
@@ -58,61 +66,6 @@ public class Context {
 
     public DrawableShape getShape(){ return shape;}
 
-    //add a method changeState that instantiates the shape as for the passed parameter
-    public void changeState(DrawableShape state){
-        shape = state; //this will be line, circle, rectangle, etc.
-    }
 
-    //add a method draw that calls the draw method of the shape class
-    public Shape draw(){
-        return shape.draw(xS,yS,xE,yE);
-    }
 
-    private static void addPersistenceDelegatesTo(Encoder encoder) {
-        encoder.setPersistenceDelegate(Font.class,
-                new DefaultPersistenceDelegate(
-                        new String[] { "name", "size" }));
-        encoder.setPersistenceDelegate(Color.class,
-                new DefaultPersistenceDelegate(
-                        new String[] { "red", "green", "blue", " black","white","transparent", "opacity" }));
-        encoder.setPersistenceDelegate(LinearGradient.class,
-                new DefaultPersistenceDelegate(new String[] {
-                        "startX", "startY", "endX", "endY",
-                        "proportional", "cycleMethod", "stops"
-                }));
-        encoder.setPersistenceDelegate(RadialGradient.class,
-                new DefaultPersistenceDelegate(new String[] {
-                        "focusAngle", "focusDistance", "centerX", "centerY",
-                        "radius", "proportional", "cycleMethod", "stops"
-                }));
-    }
-
-    public File saveFile(Pane pane, File file) throws IOException {
-        try (XMLEncoder encoder = new XMLEncoder(
-                new BufferedOutputStream(
-                        Files.newOutputStream(file.toPath())))) {
-
-            encoder.setExceptionListener(e -> {
-                throw new RuntimeException(e);
-            });
-
-            addPersistenceDelegatesTo(encoder);
-
-            encoder.writeObject(pane.getChildren().toArray(new Node[0]));
-            return file;
-        }
-    }
-
-    public File loadFile(Pane pane, File file) throws IOException {
-        try (XMLDecoder decoder = new XMLDecoder(
-                new BufferedInputStream(
-                        Files.newInputStream(file.toPath())))) {
-
-            decoder.setExceptionListener(e -> {
-                throw new RuntimeException(e);
-            });
-            pane.getChildren().setAll((Node[]) decoder.readObject());
-        }
-        return file;
-    }
 }
