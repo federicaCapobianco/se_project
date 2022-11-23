5e930001f6e479619f86bd8bfa40817819513fdc
fpastorepalumbo <f.pastorepalumbo@studenti.unisa.it>
2022-11-23 10:09:37 +0100

    add-DrawableEllipse class

---
+++ src/main/java/diem/unisa/softwareengineering/tools/DrawableEllipse.java
@@ -0,0 +1,9 @@
+package diem.unisa.softwareengineering.tools;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Shape;
+
+public class DrawableEllipse extends DrawableShape implements Drawable{
+
+
+}
