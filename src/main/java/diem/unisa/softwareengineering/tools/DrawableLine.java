e50baec5eaab1922c8d4a422b8f74c91be62990d
emilia <e.delpezzo@studenti.unisa.it>
2022-11-24 11:57:36 +0100

    edit-Pre merge

--- src/main/java/diem/unisa/softwareengineering/tools/DrawableLine.java
+++ src/main/java/diem/unisa/softwareengineering/tools/DrawableLine.java
@@ -5,6 +5,7 @@ import javafx.scene.shape.Shape;
 
 public class DrawableLine extends DrawableShape implements Drawable{
     private Shape line;
+
     @Override
     public Shape draw(double xS, double yS, double xE, double yE) {
         this.line = new Line(xS, yS, xE, yE);
