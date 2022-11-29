379c04b72219e96473a62b2228da59a3313c76f4
federicaCapobianco <capobianco.fede01@gmail.com>
2022-11-29 12:08:46 +0100

    add-copy and paste but in the wrong location

--- src/main/java/com/example/se_project/Controller.java
+++ src/main/java/com/example/se_project/Controller.java
@@ -5,10 +5,13 @@ import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.control.*;
 import javafx.scene.effect.DropShadow;
-import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.input.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -16,13 +19,20 @@ import javafx.scene.shape.*;
 import javafx.stage.FileChooser;
 import javafx.stage.Window;
 
+import java.beans.DefaultPersistenceDelegate;
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.net.URL;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 
 public class Controller implements Initializable {
-
 
     @FXML
     private Label fillLabel;
@@ -46,13 +56,25 @@ public class Controller implements Initializable {
     private Button ellipseButton;
     @FXML
     private Label tfLine;
+    @FXML
+    private Pane drawingPane;
+
+    ContextMenu contextMenu = new ContextMenu();
+    MenuItem deselect = new MenuItem("Deselect");
+    MenuItem delete = new MenuItem("Delete");
+    MenuItem move = new MenuItem("Move");
+    MenuItem lineColor = new MenuItem("Line color");
+    MenuItem fillColor = new MenuItem("Fill color");
+    MenuItem size = new MenuItem("Size");
+    MenuItem copy = new MenuItem("Copy");
+    MenuItem paste = new MenuItem("Paste");
+    DropShadow dropShadow = new DropShadow();
 
     private Tools toolManager;
     private FileManager fileManager;
     private FileChooser fileChooser;
     private Editor shapeEditor;
-    @FXML
-    private Pane drawingPane;
+    private final Clipboard clipboard = Clipboard.getSystemClipboard();
 
 
     @Override
@@ -61,6 +83,7 @@ public class Controller implements Initializable {
         fileManager = new FileManager(drawingPane);
         fileChooser = new FileChooser();
         shapeEditor = new Editor();
+
         toolManager.setShapeLineColor(Color.BLACK);
         toolManager.setShapeFillColor(Color.TRANSPARENT);
     }
@@ -80,26 +103,23 @@ public class Controller implements Initializable {
         toolManager.changeState(new DrawableRectangle());
     }
 
-    @Deprecated
+
+
+
+
+    @FXML
     public void mouseDown(MouseEvent mouseEvent) {
         if(mouseEvent.getButton() == MouseButton.SECONDARY) {
-            Shape target = (Shape) mouseEvent.getTarget();
-            shapeEditor.addSelectedNode(target);
+            Node target = (Node) mouseEvent.getTarget();
 
-            ContextMenu contextMenu = new ContextMenu();
-            MenuItem deselect = new MenuItem("Deselect");
-            MenuItem delete = new MenuItem("Delete");
-            MenuItem move = new MenuItem("Move");
-            MenuItem lineColor = new MenuItem("Line color");
-            MenuItem fillColor = new MenuItem("Fill color");
-            MenuItem size = new MenuItem("Size");
-            MenuItem copy = new MenuItem("Copy");
-            MenuItem paste = new MenuItem("Paste");
+            if(target instanceof Shape) {
+                target.setEffect(null);
+                shapeEditor.addSelectedNode((Shape)target);
+            }
 
             contextMenu.getItems().addAll(deselect, delete, move, lineColor, fillColor, size, copy, paste);
-            contextMenu.show(target, mouseEvent.getScreenX(), mouseEvent.getScreenY());
+            contextMenu.show(drawingPane, mouseEvent.getScreenX(), mouseEvent.getScreenY());
 
-            DropShadow dropShadow = new DropShadow();
             dropShadow.setRadius(5.0);
             dropShadow.setOffsetX(3.0);
             dropShadow.setOffsetY(3.0);
@@ -108,8 +128,53 @@ public class Controller implements Initializable {
             target.setEffect(dropShadow);
 
             deselect.setOnAction((ActionEvent event) -> {
-                shapeEditor.removeSelectedNode(target);
+                shapeEditor.removeSelectedNode((Shape)target);
                 target.setEffect(null);
+            });
+
+            copy.setOnAction((ActionEvent event) -> {
+                System.out.println("copy");
+                //get the file named copy.xml
+                File file = new File("copy.xml");
+                try (XMLEncoder encoder = new XMLEncoder(new BufferedOutputStream(Files.newOutputStream(file.toPath())))) {
+                    encoder.setPersistenceDelegate(Color.class, new DefaultPersistenceDelegate(new String[]{"red", "green", "blue", "opacity"}));
+                    encoder.writeObject(target);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                ClipboardContent content = new ClipboardContent();
+                List<File> files = List.of(file);
+                content.putFiles(files);
+
+                System.out.println("done copy");
+            });
+
+            paste.setOnAction((ActionEvent event) -> {
+                System.out.println("paste");
+                List<File> files = clipboard.getFiles();
+                try (XMLDecoder decoder = new XMLDecoder(
+                        new BufferedInputStream(
+                                Files.newInputStream(new File("copy.xml").toPath())))) {
+
+                    decoder.setExceptionListener(e -> {
+                        throw new RuntimeException(e);
+                    });
+
+                    Shape nodeToAdd = (Shape) decoder.readObject();
+
+                    //move the node to the mouse position
+                    nodeToAdd.setTranslateX(nodeToAdd.getTranslateX()+mouseEvent.getX());
+                    nodeToAdd.setTranslateY(nodeToAdd.getTranslateY()+mouseEvent.getY());
+                    System.out.println("node: "+nodeToAdd+"x: "+nodeToAdd.getTranslateX()+" y: "+nodeToAdd.getTranslateY());
+                    drawingPane.getChildren().add(nodeToAdd);
+                    drawingPane.getChildren().add(new Rectangle(100,100,Color.RED));
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+
+                System.out.println("done paste");
             });
         }
         else if(mouseEvent.getButton() == MouseButton.PRIMARY) {
@@ -124,7 +189,7 @@ public class Controller implements Initializable {
         //could draw a temporary shape here
     }
 
-    @Deprecated
+    @FXML
     public void mouseUp(MouseEvent mouseEvent) {
         if(mouseEvent.getButton() == MouseButton.SECONDARY) {
         }
@@ -188,11 +253,4 @@ public class Controller implements Initializable {
         toolManager.setShapeFillColor(fillColorPicker.getValue());
     }
 
-    @FXML
-    public void mouseDown(Event event) {
-    }
-
-    @FXML
-    public void mouseUp(Event event) {
-    }
 }
