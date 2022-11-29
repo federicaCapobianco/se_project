fff7a9fac7d6600a7c1558635dab1bf07edf6981
fpastorepalumbo <f.pastorepalumbo@studenti.unisa.it>
2022-11-29 10:00:54 +0100

    add - command pattern application

---
+++ src/main/java/diem/unisa/softwareengineering/tools/Command.java
@@ -0,0 +1,17 @@
+package diem.unisa.softwareengineering.tools;
+
+import javafx.scene.layout.Pane;
+
+
+
+/**
+ * A class used to apply the Command pattern that contains the abstract methods execute and undu
+ */
+public abstract class Command {
+
+    public abstract void execute();
+
+    public abstract void undo();
+
+
+}
--- src/main/java/diem/unisa/softwareengineering/tools/Editor.java
+++ src/main/java/diem/unisa/softwareengineering/tools/Editor.java
@@ -3,12 +3,17 @@ package diem.unisa.softwareengineering.tools;
 import javafx.scene.shape.Shape;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 /**
  * A class that allows to save the reference to all the right clicked(selected) nodes on the canva seen as shapes.
  */
-public class Editor {
+public class  Editor {
     private ArrayList<Shape> selectedNodes = new ArrayList<Shape>();
+
+    private Command c; //se ho lo stack qui a che serve il reference c? DA RIVEDERE
+
+    private Stack<Command> commandStack = new Stack<Command>();
 
     public ArrayList<Shape> getSelectedNodes() {
         return selectedNodes;
@@ -27,4 +32,11 @@ public class Editor {
     public void clearSelectedNodes(){
         this.selectedNodes.clear();
     }
+
+    public void setCommand() { }
+
+    public void executeCommand() { }
+
+    public void unduCommand() { }
+
 }
