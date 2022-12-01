14b323165bdeb90aa00ea3df5ade122491cea9d8
ValeriaFraenza <valeria.franza@gmail.com>
2022-12-01 16:39:19 +0100

    add-test for undo method in minus and plus size command

--- test/diem/unisa/softwareengineering/tools/MinusSizeCommandTest.java
+++ test/diem/unisa/softwareengineering/tools/MinusSizeCommandTest.java
@@ -43,6 +43,28 @@ class MinusSizeCommandTest {
 
     @Test
     void testUndo() {
+        Shape line = new Line(10,15,20,35);
+        prevX = line.getScaleX();
+        prevY = line.getScaleY();
+        cmd = new MinusSizeCommand(line);
+        cmd.execute();
+        assertTrue(line.getScaleX() > prevX);
+        assertTrue(line.getScaleY() > prevY);
 
+        Shape rect = new Rectangle(10,15,20,35);
+        prevX = rect.getScaleX();
+        prevY = rect.getScaleY();
+        cmd = new MinusSizeCommand(rect);
+        cmd.execute();
+        assertTrue(rect.getScaleX() > prevX);
+        assertTrue(rect.getScaleY() > prevY);
+
+        Shape ellipse = new Rectangle(10,15,20,35);
+        prevX = ellipse.getScaleX();
+        prevY = ellipse.getScaleY();
+        cmd = new MinusSizeCommand(ellipse);
+        cmd.execute();
+        assertTrue(ellipse.getScaleX() > prevX);
+        assertTrue(ellipse.getScaleY() > prevY);
     }
 }
--- test/diem/unisa/softwareengineering/tools/PlusSizeCommandTest.java
+++ test/diem/unisa/softwareengineering/tools/PlusSizeCommandTest.java
@@ -42,6 +42,28 @@ class PlusSizeCommandTest {
 
     @Test
     void testUndo() {
+        Shape line = new Line(10,15,20,35);
+        prevX = line.getScaleX();
+        prevY = line.getScaleY();
+        cmd = new PlusSizeCommand(line);
+        cmd.execute();
+        assertTrue(line.getScaleX() < prevX);
+        assertTrue(line.getScaleY() < prevY);
 
+        Shape rect = new Rectangle(10,15,20,35);
+        prevX = rect.getScaleX();
+        prevY = rect.getScaleY();
+        cmd = new PlusSizeCommand(rect);
+        cmd.execute();
+        assertTrue(rect.getScaleX() < prevX);
+        assertTrue(rect.getScaleY() < prevY);
+
+        Shape ellipse = new Rectangle(10,15,20,35);
+        prevX = ellipse.getScaleX();
+        prevY = ellipse.getScaleY();
+        cmd = new PlusSizeCommand(ellipse);
+        cmd.execute();
+        assertTrue(ellipse.getScaleX() < prevX);
+        assertTrue(ellipse.getScaleY() < prevY);
     }
 }
