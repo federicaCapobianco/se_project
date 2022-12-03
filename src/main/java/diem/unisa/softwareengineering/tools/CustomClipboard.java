2e504243288643f15ee4ecc4a77229c3f47c0517
federicaCapobianco <capobianco.fede01@gmail.com>
2022-12-03 10:13:18 +0100

    fix-copy and paste removing the methods from editor perform-customClipboard tests

---
+++ src/main/java/diem/unisa/softwareengineering/tools/CustomClipboard.java
@@ -0,0 +1,52 @@
+package diem.unisa.softwareengineering.tools;
+
+import javafx.scene.Node;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Shape;
+
+import java.beans.DefaultPersistenceDelegate;
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.nio.file.Files;
+import java.util.List;
+
+public class CustomClipboard {
+
+    public void copy(Node shape){
+        File file = new File("copy.xml");
+        shape.setEffect(null);
+        try (XMLEncoder encoder = new XMLEncoder(new BufferedOutputStream(Files.newOutputStream(file.toPath())))) {
+            encoder.setPersistenceDelegate(Color.class, new DefaultPersistenceDelegate(new String[]{"red", "green", "blue", "opacity"}));
+            encoder.writeObject(shape);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Shape paste(Pane drawingPane, double selectionPointX, double selectionPointY){
+        try (XMLDecoder decoder = new XMLDecoder(
+                new BufferedInputStream(
+                        Files.newInputStream(new File("copy.xml").toPath())))) {
+
+            decoder.setExceptionListener(e -> {
+                throw new RuntimeException(e);
+            });
+
+            Shape nodeToAdd = (Shape) decoder.readObject();
+
+            nodeToAdd.relocate(selectionPointX, selectionPointY);
+            drawingPane.getChildren().add(nodeToAdd);
+            return nodeToAdd;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
