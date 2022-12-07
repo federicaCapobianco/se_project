374660450702416916abd94aa79ec005ef493cbd
ValeriaFraenza <valeria.franza@gmail.com>
2022-12-07 11:07:31 +0100

    add-DrawablePolygonTest class

---
+++ test/diem/unisa/softwareengineering/tools/DrawablePolygonTest.java
@@ -0,0 +1,17 @@
+package diem.unisa.softwareengineering.tools;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DrawablePolygonTest {
+    @Test
+    void testDraw(){
+
+    }
+    @Test
+    void testClosureDraw(){
+
+    }
+
+}
