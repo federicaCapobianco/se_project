90c13f80a8d9e87933e8e861a71465b602fdeaff
ValeriaFraenza <valeria.franza@gmail.com>
2022-12-07 12:05:19 +0100

    add-testDraw and testClosureDraw methods

--- test/diem/unisa/softwareengineering/tools/DrawablePolygonTest.java
+++ test/diem/unisa/softwareengineering/tools/DrawablePolygonTest.java
@@ -1,17 +1,46 @@
 package diem.unisa.softwareengineering.tools;
 
+import javafx.scene.shape.Shape;
 import org.junit.jupiter.api.Test;
+
+import javafx.scene.paint.Color;
+
+import java.awt.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DrawablePolygonTest {
     @Test
     void testDraw(){
+        DrawablePolygon poly = new DrawablePolygon();
 
+        assertInstanceOf(Shape.class, poly.draw(0.0, 0.0, 2.0, 3.0, Color.BLACK, Color.YELLOW));
+        assertNotEquals(Color.YELLOW, poly.draw(0.0, 0.0, 2.0, 3.0, Color.BLACK, Color.YELLOW).getFill());
+        assertEquals(Color.BLACK, poly.draw(0.0, 0.0, 2.0, 3.0, Color.BLACK, Color.YELLOW).getFill());
+
+        assertInstanceOf(Shape.class, poly.draw(0.0, 5.0, 2.0, 3.0, Color.BLACK, Color.YELLOW));
+        assertNotEquals(Color.YELLOW, poly.draw(0.0, 5.0, 2.0, 3.0, Color.BLACK, Color.YELLOW).getFill());
+        assertEquals(Color.BLACK, poly.draw(0.0, 5.0, 2.0, 3.0, Color.BLACK, Color.YELLOW).getFill());
+
+        assertInstanceOf(Shape.class, poly.draw(5.0, 5.0, 2.0, 3.0, Color.BLACK, Color.YELLOW));
+        assertNotEquals(Color.YELLOW, poly.draw(5.0, 5.0, 2.0, 3.0, Color.BLACK, Color.YELLOW).getFill());
+        assertEquals(Color.BLACK, poly.draw(5.0, 5.0, 2.0, 3.0, Color.BLACK, Color.YELLOW).getFill());
+        //i due assert finali serve per verificare che non sia una shape chiusa
+        //verificando la non esecuzione della setFill (con colore YELLOW)
+        //essendo impostato il valore di default (con colore BLACK)
     }
     @Test
     void testClosureDraw(){
+        DrawablePolygon poly = new DrawablePolygon();
 
+        poly.draw(0.0, 0.0, 2.0, 3.0, Color.BLACK, Color.YELLOW);
+        poly.draw(0.0, 5.0, 2.0, 3.0, Color.BLACK, Color.YELLOW);
+        poly.draw(5.0, 5.0, 2.0, 3.0, Color.BLACK, Color.YELLOW);
+
+        assertInstanceOf(Shape.class, poly.closureDraw(Color.BLACK, Color.YELLOW));
+        assertEquals(Color.YELLOW, poly.closureDraw(Color.BLACK, Color.YELLOW).getFill());
+        //il secondo assert serve per verificare che sia una shape chiusa
+        //verificando l'esecuzione della setFill (con colore YELLOW)
     }
 
 }
